081c6081563a99cc77a10262dbaa6d2f577586cd
v1nland <martin.saavedra@mail.udp.cl>
2019-04-10 08:12:44 -0400

    Initial commit. (#1)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Express-Mongo-C3
 Simple backend server, made for Cobra III proyect.
+
+To install all dependencies, use the following command:
+
+```
+npm install
+```
+
+Please note you need Node.JS installed on your PC.
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+
+// Mongoose database connection
+mongoose.connect('mongodb://localhost/worldinapetri', {useNewUrlParser: true});
+
+//CORS stuff
+app.use(cors());
+app.use(function (req, res, next) {
+    // Grant access to users
+    var origin = req.headers.origin;
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
+// LANDING
+app.get('/', (req, res) => {
+    res.send("Hello from Cobra III server");
+});
+
+// SEARCH FOR ALL AMBIENTS
+mongoose.model('ambients', {id: Number, name: String});
+app.get('/ambients', (req, res) => {
+    mongoose.model('ambients').find(function(err, ambients){
+        res.send(ambients);
+    })
+});
+
+// Console stuff
+var port = process.env.PORT || 27019;
+app.listen(port, "0.0.0.0", () => {
+    console.log('Cobra III server listening on port ' + port );
+});
